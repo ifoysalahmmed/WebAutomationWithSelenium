f27b96100965431aacf318034f5221f36aecab0f
Foysal Ahmmed Limon <afaysal410@gmail.com>
2025-01-05 14:14:54 +0600

    added DriverSetup class

--- src/test/java/org/example/BrowseAWebPage.java
+++ src/test/java/org/example/BrowseAWebPage.java
@@ -1,6 +1,18 @@
 package org.example;
 
-public class BrowseAWebPage {
-    
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BrowseAWebPage extends DriverSetup {
+    @Test
+    public void loadAPage() {
+        // load URL
+        driver.get("https://www.google.com");
+        Assert.assertEquals(driver.getTitle(), "Google");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.google.com/");
+
+
+    }
 }
-~
