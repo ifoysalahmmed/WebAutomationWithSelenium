634244248cceb7a994d5bbe4cfe00fb1235d866b
Foysal Ahmmed Limon <afaysal410@gmail.com>
2025-01-05 14:17:29 +0600

    added DriverSetup & BrowseAWebPage classes

---
+++ src/test/java/org/example/DriverSetup.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class DriverSetup {
+
+    public WebDriver driver;
+
+
+    @BeforeSuite
+    public void openBrowser() {
+        // Open a browser
+        driver = new ChromeDriver();
+    }
+
+    @AfterSuite
+    public void closeBrowser() {
+        // Close browser
+        driver.quit();
+    }
+}
